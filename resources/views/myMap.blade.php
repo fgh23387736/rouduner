e4bb81716787fcfead78ff191b5e98a96c3b7a68
fgh23387736 <23387736@qq.com>
2018-07-31 23:46:35 +0800

    ..

--- resources/views/myMap.blade.php
+++ resources/views/myMap.blade.php
@@ -18,7 +18,7 @@
 <script type="text/javascript">
     // 百度地图API功能
     var map = new BMap.Map("allmap");    // 创建Map实例
-    map.centerAndZoom(new BMap.Point(118.195087,39.646809), 13);  // 初始化地图,设置中心点坐标和地图级别
+    map.centerAndZoom(new BMap.Point(118.195087,39.646809), 10);  // 初始化地图,设置中心点坐标和地图级别
     //添加地图类型控件
     map.addControl(new BMap.MapTypeControl({
         mapTypes:[
